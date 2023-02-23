c2dfc6d7c87a9ce8b37aa431610effd616054f35
Joe Crouse <jsaints97@hotmail.com>
2023-02-22 19:26:16 -0600

    Updated README and uncommented python code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # First-API
 First time using an api in python.
+Added api to call the names of the astronauts that are currently in space.
+Added api which returns the coordinates of the current location of the ISS.
--- my_api.py
+++ my_api.py
@@ -1,19 +1,19 @@
 import requests
-# request = requests.get("http://api.open-notify.org")
-# #print(request.text)
-# #print(request.status_code)
-# request2 = requests.get('http://api.open-notify.org/fake-endpoint')
-# #print(request2.status_code)
-# people = requests.get('http://api.open-notify.org/astros.json')
-# #print(people.text)
-# people_json = people.json()
-# #print(people_json)
+request = requests.get("http://api.open-notify.org")
+#print(request.text)
+#print(request.status_code)
+request2 = requests.get('http://api.open-notify.org/fake-endpoint')
+#print(request2.status_code)
+people = requests.get('http://api.open-notify.org/astros.json')
+#print(people.text)
+people_json = people.json()
+#print(people_json)
 
-# #To print the number of people in space
-# print("Number of people in space:",people_json['number'])
-# #To print the names of people in space using a for loop
-# for p in people_json['people']:
-#     print(p['name'])
+#To print the number of people in space
+print("Number of people in space:",people_json['number'])
+#To print the names of people in space using a for loop
+for p in people_json['people']:
+    print(p['name'])
 
 
 iss_now = requests.get('http://api.open-notify.org/iss-now.json')
